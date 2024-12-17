b65e7b7c92f4221060a17e6103d683c7532fb22a
Raseel <raseelp321@gmail.com>
2024-12-17 18:21:19 +0530

    Setup The FlutterApp And Integrated The Firebase

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeePage extends StatelessWidget {
+  const HomeePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('HomePage')),
+    );
+  }
+}

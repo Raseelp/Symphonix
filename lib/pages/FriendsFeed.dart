06dd55ea1fc468eb2b6395694a3a7fc7f43b6ce0
Raseel <raseelp321@gmail.com>
2024-12-21 09:44:30 +0530

    Create FriendsFeed.dart

---
+++ lib/pages/FriendsFeed.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class FriednsFeed extends StatefulWidget {
+  const FriednsFeed({super.key});
+
+  @override
+  State<FriednsFeed> createState() => _FriednsFeedState();
+}
+
+class _FriednsFeedState extends State<FriednsFeed> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
